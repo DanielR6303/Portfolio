3fb993d7d9a766a8d5f1b2e56b3456fce42ed755
DanielR6303 <danielr6303@gmail.com>
2024-03-14 11:28:36 +0530

    error cleared

--- src/components/About/info.jsx
+++ src/components/About/info.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
     <div className="about-info grid">
-        <div className="about-box">
+      <div className="about-box">
         <i class="uil uil-book-open about-icon"></i>
-            <h3 className="about-title"> Education</h3>
-                <spam className="about-subtitle">BSc.Computer science, 2020-2023</spam>
-           
-        </div>
+        <h3 className="about-title"> Education</h3>
+        <spam className="about-subtitle">BSc.Computer science, 2020-2023</spam>
+      </div>
 
-        <div className="about-box">
+      <div className="about-box">
         <i class="uil uil-briefcase-alt about-icon"></i>
-            <h3 className="about-title">Completed </h3>
-                <spam className="about-subtitle">3 projects</spam>
-           
-        </div>
+        <h3 className="about-title">Completed </h3>
+        <spam className="about-subtitle">3 projects</spam>
+      </div>
 
-        <div className="about-box">
+      <div className="about-box">
         <i class="bx bx-support about-icon"></i>
-            <h3 className="about-title">Support </h3>
-                <spam className="about-subtitle">Online 24/7</spam>
-           
-        </div>
+        <h3 className="about-title">Support </h3>
+        <spam className="about-subtitle">Online 24/7</spam>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
